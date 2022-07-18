5a3f4753b524ce827fc3adfe132efe89e53faef8
VladimirZhdanov <ConstHomespb@gmail.com>
2022-07-18 23:01:30 +0200

    refactoring DataServiceImpl

--- src/main/java/com/homel/demo/project/service/impl/DataServiceImpl.java
+++ src/main/java/com/homel/demo/project/service/impl/DataServiceImpl.java
@@ -6,6 +6,7 @@ import com.homel.demo.project.repository.DataRepository;
 import com.homel.demo.project.service.DataService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,31 +16,37 @@ public class DataServiceImpl implements DataService {
     private DataRepository dataRepository;
 
     @Override
+    @Transactional
     public void save(Instructor instructor) {
         dataRepository.save(instructor);
     }
 
     @Override
+    @Transactional
     public Instructor getInstructor(long id) {
         return dataRepository.getInstructor(id);
     }
 
     @Override
+    @Transactional
     public void update(Instructor instructor) {
         dataRepository.update(instructor);
     }
 
     @Override
+    @Transactional
     public void deleteStudent(long id) {
         dataRepository.deleteStudent(id);
     }
 
     @Override
+    @Transactional
     public List<Instructor> getAllInstructors() {
         return dataRepository.getAllInstructors();
     }
 
     @Override
+    @Transactional
     public List<Course> getAllCourses() {
         return dataRepository.getAllCourses();
     }
